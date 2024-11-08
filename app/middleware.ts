d7f94fe1c29739acdbf90aed7c3e98676f34c1dd
JOEY A. DELOS SANTOS <JOADELOSSANTOS@palawanpawnshop.com>
2024-11-08 17:00:52 +0800

    Fresh start

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,7 +1,5 @@
 import { NextApiResponse, type NextApiRequest } from "next";
 import { type NextRequest , NextResponse } from "next/server";
-
-
      
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
@@ -10,15 +8,5 @@ export function middleware(request: NextRequest) {
     return NextResponse.redirect(new URL("/login", request.url))
     
   }
-   
-  // See "Matching Paths" below to learn more
 
-    // if(request.nextUrl.pathname === '/'){
-    //     return Response.redirect(new URL('/login', request.url))    
-    // }
-
-    // else if(request.nextUrl.pathname === '/login'){
-    //     return Response.redirect(new URL('/registration', request.url))    
-    // }
 }
-  
